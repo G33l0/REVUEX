cad96a9579ac5aac85fc55094a714bff95be38e6
G33l0 <121323412+G33l0@users.noreply.github.com>
2025-12-19 12:10:05 -0600

    Update __init__.py

--- core/__init__.py
+++ core/__init__.py
@@ -51,35 +51,222 @@ from .base_scanner import (
     print_disclaimer,
 )
 
+from .safety_checks import (
+    # Main classes
+    SafetyManager,
+    ScopeValidator,
+    PayloadValidator,
+    RequestValidator,
+    DNSRebindingProtector,
+    
+    # Data classes
+    ScopeDefinition,
+    SafetyCheckResult,
+    
+    # Enums
+    SafetyLevel,
+    ValidationResult,
+    
+    # Utility functions
+    is_private_ip,
+    is_cloud_metadata,
+    extract_base_domain,
+    create_default_scope,
+    
+    # Constants
+    PRIVATE_IP_RANGES,
+    CLOUD_METADATA_ENDPOINTS,
+    DANGEROUS_PORTS,
+    SENSITIVE_PATHS,
+)
+
+from .utils import (
+    # URL utilities
+    normalize_url,
+    parse_url,
+    build_url,
+    extract_domain,
+    extract_base_url,
+    extract_parameters,
+    inject_parameter,
+    is_same_origin,
+    
+    # Encoding utilities
+    url_encode,
+    url_decode,
+    base64_encode,
+    base64_decode,
+    html_encode,
+    html_decode,
+    to_json,
+    from_json,
+    
+    # Hashing utilities
+    md5,
+    sha1,
+    sha256,
+    generate_hash_id,
+    
+    # String utilities
+    random_string,
+    random_hex,
+    truncate,
+    clean_string,
+    extract_between,
+    similarity_ratio,
+    
+    # Data extraction
+    extract_emails,
+    extract_urls,
+    extract_ips,
+    extract_secrets,
+    PATTERNS,
+    
+    # File utilities
+    read_file,
+    write_file,
+    read_json_file,
+    write_json_file,
+    ensure_dir,
+    file_exists,
+    
+    # Network utilities
+    is_valid_ip,
+    resolve_hostname,
+    is_success_status,
+    is_error_status,
+    
+    # Time utilities
+    get_timestamp,
+    format_duration,
+    sleep_with_jitter,
+    
+    # Color/formatting
+    Colors,
+    print_success,
+    print_error,
+    print_warning,
+    print_info,
+    
+    # Decorators
+    retry,
+    memoize,
+    timed,
+)
+
 # Will be populated as we create more core modules
-# from .safety_checks import SafetyValidator
 # from .logger import RevuexLogger
 # from .intelligence_hub import IntelligenceHub
 # from .report_generator import ReportGenerator
-# from .utils import *
 
 __version__ = REVUEX_VERSION
 __author__ = "REVUEX Team"
 
 __all__ = [
+    # === base_scanner.py ===
     # Core classes
     "BaseScanner",
     "Finding",
     "ScanResult",
     "RateLimiter",
-    
     # Enums
     "Severity",
     "ScanStatus",
     "RequestMethod",
-    
     # Constants
     "REVUEX_VERSION",
     "REVUEX_BANNER",
     "DEFAULT_CONFIG",
     "USER_AGENTS",
-    
     # Utility functions
     "get_scanner_info",
     "print_disclaimer",
+    
+    # === safety_checks.py ===
+    # Main classes
+    "SafetyManager",
+    "ScopeValidator",
+    "PayloadValidator",
+    "RequestValidator",
+    "DNSRebindingProtector",
+    # Data classes
+    "ScopeDefinition",
+    "SafetyCheckResult",
+    # Enums
+    "SafetyLevel",
+    "ValidationResult",
+    # Utility functions
+    "is_private_ip",
+    "is_cloud_metadata",
+    "extract_base_domain",
+    "create_default_scope",
+    # Constants
+    "PRIVATE_IP_RANGES",
+    "CLOUD_METADATA_ENDPOINTS",
+    "DANGEROUS_PORTS",
+    "SENSITIVE_PATHS",
+    
+    # === utils.py ===
+    # URL utilities
+    "normalize_url",
+    "parse_url",
+    "build_url",
+    "extract_domain",
+    "extract_base_url",
+    "extract_parameters",
+    "inject_parameter",
+    "is_same_origin",
+    # Encoding utilities
+    "url_encode",
+    "url_decode",
+    "base64_encode",
+    "base64_decode",
+    "html_encode",
+    "html_decode",
+    "to_json",
+    "from_json",
+    # Hashing utilities
+    "md5",
+    "sha1",
+    "sha256",
+    "generate_hash_id",
+    # String utilities
+    "random_string",
+    "random_hex",
+    "truncate",
+    "clean_string",
+    "extract_between",
+    "similarity_ratio",
+    # Data extraction
+    "extract_emails",
+    "extract_urls",
+    "extract_ips",
+    "extract_secrets",
+    "PATTERNS",
+    # File utilities
+    "read_file",
+    "write_file",
+    "read_json_file",
+    "write_json_file",
+    "ensure_dir",
+    "file_exists",
+    # Network utilities
+    "is_valid_ip",
+    "resolve_hostname",
+    "is_success_status",
+    "is_error_status",
+    # Time utilities
+    "get_timestamp",
+    "format_duration",
+    "sleep_with_jitter",
+    # Color/formatting
+    "Colors",
+    "print_success",
+    "print_error",
+    "print_warning",
+    "print_info",
+    # Decorators
+    "retry",
+    "memoize",
+    "timed",
 ]
